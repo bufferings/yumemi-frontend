908ce33f8bddf25daa0feb0509ca6320836da527
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-06-04 21:24:12 +0900

    Suppress warning of msw unhandled request.

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import { App } from 'src/pages/App';
 
 if (import.meta.env.DEV && !import.meta.env.VITE_WITHOUT_MSW) {
   const { worker } = await import('./mocks/browser');
-  await worker.start();
+  await worker.start({
+    onUnhandledRequest: 'bypass',
+  });
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
