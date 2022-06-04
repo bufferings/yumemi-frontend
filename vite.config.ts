451c867a340dd437d9ac38fe78167f75b619ca67
Mitsuyuki Shiiba <bufferings@gmail.com>
2022-06-04 22:20:39 +0900

    Change to allow top level await

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   base: '/yumemi-frontend/',
   plugins: [react(), tsconfigPaths()],
+  build: { target: 'esnext' },
 });
